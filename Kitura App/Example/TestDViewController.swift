f07f433caffe8ae645ea1247ec375cb351350089
Linhao CUI 崔林豪 <linhao.cui@nio.com>
2023-05-09 20:33:29 +0800

    添加证书测试

--- Kitura App/Example/TestDViewController.swift
+++ Kitura App/Example/TestDViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import Vapor
 import NIOFoundationCompat
+import NIOSSL
+
 
 // Mark: - Models
 
@@ -85,8 +87,91 @@ extension TestDViewController {
         //0.0.0.0是监听ipv4 的任意端口
         try! app.server.start(address: .hostname("0.0.0.0", port: 8080))
     }
-    
+    //testCanOverrideCertValidation
+    ///测试用证书请求
     func test3() {
+        let app = Application(.testing)
+        app.routes.defaultMaxBodySize = ByteCount(value: Int.max)
+        
+        //let ssl = SSLConfig(withChainFilePath: Bundle.main.path(forResource: "Certificates", ofType: "p12"), withPassword: "123456789")
+        
+        
+        let cert = try! NIOSSLCertificate(file: Bundle.main.path(forResource: "Certificate", ofType: "pem")!, format: .pem)
+        let key = try! NIOSSLPrivateKey(file: Bundle.main.path(forResource: "PrivateKey", ofType: "pem")!, format: .pem)
+        
+        app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+            certificateChain: [.certificate(cert)],
+            privateKey: .privateKey(key)
+        )
+        
+        app.get("hello") { req in
+            return "OK"
+        }
+        app.on(.PUT, "upload", body: .stream) { req in
+            let done = req.eventLoop.makePromise(of: Response.self)
+            let size = Double(req.headers["Content-Length"].first!)!
+            print(size)
+            
+            //let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(UUID().uuidString, isDirectory: false)
+            let fileManager = FileManager.default
+            var url = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("MP4s", isDirectory: true)
+            ///创建文件
+            try fileManager.createDirectory(at: url, withIntermediateDirectories: true)
+            
+            ///创建文件名
+            url.appendPathComponent("received_file.mp4", isDirectory: false)
+            //url.appendPathComponent("received_file.ts", isDirectory: false)
+            debugPrint("@@_ add file tp: \(url)")
+            try? fileManager.removeItem(at: url)
+            fileManager.createFile(atPath: url.path, contents: nil)
+            
+            let fileHandle = try FileHandle(forWritingTo: url)
+                                                                                   
+            print("__>>>\(url.path)")
+            //let handle = OutputStream(url: url, append: false)!
+            
+            var total: Double = 0
+            req.body.drain { result in
+                let promise = req.eventLoop.makePromise(of: Void.self)
+                //768163530
+                switch result {
+                case .buffer(let buffer):
+                    total += Double(Data(buffer: buffer).count)
+                    //Data(buffer: buffer)
+                    let ff = Data(buffer: buffer)
+                    //这个会crash
+                    //func write<T>(contentsOf data: T) throws where T : DataProtocol
+                    //这个api最多支持iOS13.4 故不可用
+                    // fileHandle.write(ff) 这个方法会给OC抛出一个异常，swift捕获不到，就会crash
+                    fileHandle.write(ff)
+                    print("\(total / size * 100)")
+                    promise.succeed(())
+                case .error(let error):
+                    done.fail(error)
+                    
+                case .end:
+                    //handle.close()
+                   try? fileHandle.close()
+                    promise.succeed(())
+                    done.succeed(Response(status: .created))
+                }
+                
+                // manually return pre-completed future
+                // this should balloon in memory
+                // return req.eventLoop.makeSucceededFuture(())
+                
+                // return real future that indicates bytes were handled
+                // this should use very little memory
+                return promise.futureResult
+            }
+            return done.futureResult
+        }
+        
+        //0.0.0.0是监听ipv4 的任意端口
+        try! app.server.start(address: .hostname("0.0.0.0", port: 8080))
+    }
+    
+    func test33() {
         let app = Application(.testing)
         app.routes.defaultMaxBodySize = ByteCount(value: Int.max)
         /*
@@ -192,6 +277,7 @@ extension TestDViewController {
         try! app.server.start(address: .hostname("0.0.0.0", port: 8080))
     }
     
+    ///处理接收数据时候回调的报错
     func handler_error() {
         let app = Application(.testing)
         app.routes.defaultMaxBodySize = ByteCount(value: Int.max)
@@ -305,7 +391,7 @@ extension TestDViewController {
         try! app.server.start(address: .hostname("0.0.0.0", port: 8080))
     }
     
-    func test33() {
+    func read_buffer() {
         let app = Application(.testing)
         app.routes.defaultMaxBodySize = ByteCount(value: Int.max)
         /*
